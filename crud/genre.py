a36a507c8503326508395fce7d601a559f591732
Fabian Noll <Fabian.Noll@googlemail.com>
2023-03-31 13:59:46 +0200

    Moved to keep structure

--- crud/genre.py
+++ crud/genre.py
@@ -12,6 +12,20 @@ def get_genre(db: Session, genre: str) -> Genre:
     return result
 
 
+def get_genre_by_id(db: Session, genre_id: int) -> Genre:
+    result: Union[models.Genre, None] = db.query(models.Genre).filter(models.Genre.id == genre_id).one_or_none()
+    return result
+
+
+def get_genres_by_manga_id(db: Session, manga_id: int) -> List[Genre]:
+    genres: List[Genre] = []
+    result: List[Type[models.RelationMangaGenre]] = db.query(models.RelationMangaGenre).filter(models.RelationMangaGenre.mangaID == manga_id).all()
+    for relation in result:
+        genre = get_genre_by_id(db, relation.genreID)
+        genres.append(genre)
+    return genres
+
+
 def create_genre(db: Session, genre_name: str) -> Genre:
     genre = models.Genre()
     genre.name = genre_name
@@ -29,31 +43,3 @@ def create_relation(db: Session, genre_id: int, manga_id: int):
 
     db.add(relation)
     db.commit()
-
-
-def get_relations_by_manga(db: Session, manga_id: int):
-    raise HTTPException(
-        status_code=501,
-        detail="The Developer needed some coffee"
-    )
-
-
-def get_relations_by_genre(db: Session, genre_id: int):
-    raise HTTPException(
-        status_code=501,
-        detail="The Developer needed some coffee"
-    )
-
-
-def get_genre_by_id(db: Session, genre_id: int) -> Genre:
-    result: Union[models.Genre, None] = db.query(models.Genre).filter(models.Genre.id == genre_id).one_or_none()
-    return result
-
-
-def get_genres_by_manga_id(db: Session, manga_id: int) -> List[Genre]:
-    genres: List[Genre] = []
-    result: List[Type[models.RelationMangaGenre]] = db.query(models.RelationMangaGenre).filter(models.RelationMangaGenre.mangaID == manga_id).all()
-    for relation in result:
-        genre = get_genre_by_id(db, relation.genreID)
-        genres.append(genre)
-    return genres
