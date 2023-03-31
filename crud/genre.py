5d93524ffa91cccef71a8bd7e8fae660d091eda5
Fabian Noll <Fabian.Noll@googlemail.com>
2023-03-31 11:02:56 +0200

    Moved crud relationship operation to genre crud operations

--- crud/genre.py
+++ crud/genre.py
@@ -9,15 +9,36 @@ from models import Genre
 
 def get_genre(db: Session, genre: str) -> Genre:
     result: Union[models.Genre, None] = db.query(models.Genre).filter(models.Genre.name == genre).one_or_none()
-    if result is None:
-        raise HTTPException(
-            status_code=404,
-            detail="Genre not found"
-        )
     return result
 
 
-def create_genre(db: Session, genre: str):
+def create_genre(db: Session, genre_name: str) -> Genre:
+    genre = models.Genre()
+    genre.name = genre_name
+    db.add(genre)
+    db.commit()
+    db.refresh(genre)
+
+    return genre
+
+
+def create_relation(db: Session, genre_id: int, manga_id: int):
+    relation = models.RelationMangaGenre()
+    relation.mangaID = manga_id
+    relation.genreID = genre_id
+
+    db.add(relation)
+    db.commit()
+
+
+def get_relations_by_manga(db: Session, manga_id: int):
+    raise HTTPException(
+        status_code=501,
+        detail="The Developer needed some coffee"
+    )
+
+
+def get_relations_by_genre(db: Session, genre_id: int):
     raise HTTPException(
         status_code=501,
         detail="The Developer needed some coffee"
