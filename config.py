1780aa26bca0a31f9a2f994c6a7b99940f6edc1b
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-02 20:50:14 +0200

    Add config.py to read in private credentials

---
+++ config.py
@@ -0,0 +1,14 @@
+import json
+
+MYANIMELIST_ACCESS_TOKEN = ''
+
+
+def read_token_json():
+    global MYANIMELIST_ACCESS_TOKEN
+    with open('token.json', 'r') as f:
+        data = json.load(f)
+        MYANIMELIST_ACCESS_TOKEN = data["token"]
+
+
+def read():
+    read_token_json()
