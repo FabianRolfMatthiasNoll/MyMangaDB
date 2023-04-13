462fc408a02a00a417c12d77a698485d43086332
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-13 21:56:53 +0200

    Remove standard stuff and testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { Button } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+// https://mui.com/material-ui/getting-started/installation/
+// https://github.com/callFEELD/react-example
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Button variant="contained" endIcon={<SendIcon />}>
+            Send
+          </Button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
-
 export default App;
