90e2a6e0d798bf0e1d2874d6d52bde6c54f5e4bf
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-14 07:14:20 +0200

    Add Basic Dashboard Component

---
+++ frontend/src/listItems.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import PeopleIcon from "@mui/icons-material/People";
+import BookIcon from "@mui/icons-material/Book";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <BookIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Authors" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AccountTreeIcon />
+      </ListItemIcon>
+      <ListItemText primary="Genres" />
+    </ListItemButton>
+  </React.Fragment>
+);
