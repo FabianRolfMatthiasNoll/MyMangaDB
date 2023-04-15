d9184598270f9e00bf1ac9e6292b31a719e0aa2e
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-15 16:47:28 +0200

    Add cors support

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import config
 import models
 from database import engine
@@ -8,6 +9,19 @@ app = FastAPI()
 
 app.include_router(manga.router)
 app.include_router(mal.router)
+
+# CORS settings
+origins = [
+    "http://192.168.56.1:3000",  # Add your frontend origin here
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
