568028375781f64c62e5ffbd9dc4c334d97d52da
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-16 13:46:49 +0200

    Display Mangas now as cards

--- frontend/src/MangaList.tsx
+++ frontend/src/MangaList.tsx
@@ -1,6 +1,14 @@
 import { components, operations } from "./api-types";
 import { useState, useEffect } from "react";
 import axios, { AxiosResponse } from "axios";
+import Grid from "@mui/material/Grid";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import volumecover from "./static/images/volumecover.jpg";
 
 type Manga = components["schemas"]["Manga"];
 const API_URL = "http://localhost:8000";
@@ -30,7 +38,40 @@ const MangaList: React.FC = () => {
   }
 
   return (
-    <div>
+    <Grid container spacing={1}>
+      {mangas.map((manga, index) => (
+        <Grid xs={3}>
+          <Card sx={{ maxWidth: 270 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="400"
+                image={volumecover}
+                alt="mangacover"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {manga.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Authors:{" "}
+                  {manga.authors
+                    .map((author) => `${author.name} (${author.role})`)
+                    .join(", ")}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default MangaList;
+
+{
+  /* <div>
       <h1>All Mangas</h1>
       {mangas.map((manga, index) => (
         <div key={index}>
@@ -46,8 +87,5 @@ const MangaList: React.FC = () => {
           </p>
         </div>
       ))}
-    </div>
-  );
-};
-
-export default MangaList;
+    </div> */
+}
