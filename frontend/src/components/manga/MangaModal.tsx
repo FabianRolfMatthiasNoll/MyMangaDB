efa9710d5cdd93ac55e6e5f0e7ccf9f35672a65c
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-19 16:50:18 +0200

    Refactor component to properly use mui style

--- frontend/src/components/manga/MangaModal.tsx
+++ frontend/src/components/manga/MangaModal.tsx
@@ -1,3 +1,6 @@
+import { Box, Button, Modal, Typography } from "@mui/material";
+import { Manga } from "../../api/models";
+import { useState } from "react";
 import { Button, Modal } from "@mui/material";
 import { Manga, Author, Genre } from "../../api/models";
 
@@ -10,29 +13,71 @@ interface Props {
 export default function MangaModal({ manga, open, onClose }: Props) {
   const imageUrl = `/static/images/${manga.title.replace(":", "")}_cover.jpg`;
   return (
-    <Modal open={open} onClose={onClose}>
-      <div className="modal-container">
-        <div className="modal-header">
-          <h2>{manga.title}</h2>
-          <Button onClick={onClose}>X</Button>
-        </div>
-        <div className="modal-content">
-          <img className="modal-cover" src={imageUrl} alt="mangacover" />
-          <div className="modal-text">
-            <p>
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="manga-modal-title"
+      aria-describedby="manga-modal-description"
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          bgcolor: "background.paper",
+          boxShadow: 24,
+          p: 4,
+          borderRadius: 5,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={imageUrl} alt="mangacover" />
+          <Box sx={{ ml: 5 }}>
+            <Typography
+              id="manga-modal-title"
+              variant="h2"
+              component="h2"
+              gutterBottom
+            >
+              {manga.title}
+            </Typography>
+            <Typography component="p" mb={1}>
               <strong>Authors:</strong>{" "}
               {manga.authors
-                .map((author: Author) => `${author.name} (${author.role})`)
+                .map((author) => `${author.name} (${author.role})`)
                 .join(", ")}
-            </p>
-            <p>
+            </Typography>
+            <Typography component="p" mb={1}>
               <strong>Genres:</strong>{" "}
+              {manga.genres.map((genre) => genre.name).join(", ")}
+            </Typography>
+            <Typography
+              id="manga-modal-description"
+              component="p"
+              mt={1}
+              sx={{
+                whiteSpace: "pre-line",
+              }}
+            >
+              <strong>Description:</strong>
+              {"\n"} {manga.description}
+            </Typography>
+          </Box>
+        </Box>
               {manga.genres.map((genre: Genre) => genre.name).join(", ")}
             </p>
             <p>{manga.description}</p>
           </div>
         </div>
       </div>
+      </Box>
     </Modal>
   );
 }
