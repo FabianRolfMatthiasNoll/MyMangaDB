7ad1f9b7007a3c0329cb4a4a695b0ec0a9053bae
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-19 21:13:32 +0200

    Add openapi template to keep working from workstation

---
+++ frontend/src/api/apis/index.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './MangaApi';
+export * from './MyAnimeListApi';
---
+++ frontend/src/api/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Author';
+export * from './Genre';
+export * from './HTTPValidationError';
+export * from './LocationInner';
+export * from './Manga';
+export * from './ValidationError';
