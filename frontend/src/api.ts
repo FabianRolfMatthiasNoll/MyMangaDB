b8d2c6f6f7765553029b27a9e03d0e120f53f4d1
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-20 14:37:20 +0200

    Make MAL api route usable

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,5 @@
-import { MangaApi } from "./api/apis";
+import { MangaApi, MyAnimeListApi } from "./api/apis";
 
 export const mangaAPI = new MangaApi();
+
+export const mangaMALAPI = new MyAnimeListApi();
