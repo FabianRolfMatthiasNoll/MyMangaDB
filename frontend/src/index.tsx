96eb405593b5ba9581dd67ec8b4e683828138e4c
Fabian Noll <Fabian.Noll@googlemail.com>
2023-04-26 22:32:18 +0200

    Make Scrollbar more fitting in Design

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./scrollbarStyles.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
