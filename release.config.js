35f6785c04cc3a3c3ca18b830f568b0dcc676f48
Fabian Noll <Fabian.Noll@googlemail.com>
2025-12-03 13:53:31 +0100

    feat: implement semantic release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ["master", "main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["CHANGELOG.md", "package.json"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
+};
