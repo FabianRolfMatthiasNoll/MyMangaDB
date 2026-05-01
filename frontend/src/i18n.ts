7a0f99c0e02849b26291a885f74a01884fc2af3b
Fabian Noll <Fabian.Noll@googlemail.com>
2026-05-01 13:54:21 +0200

    feat(frontend): add translation library

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import en from './locales/en.json';
+import de from './locales/de.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      de: { translation: de },
+    },
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'de'],
+    detection: {
+      order: ['localStorage', 'navigator'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
